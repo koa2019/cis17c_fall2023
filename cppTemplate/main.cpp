feff10308331a923bba7ae385c7ae967002a2f2e
koa2019 <no-reply>
2023-10-27 00:53:40 -0700

    Adding Linked list program, permutation and recursion comparisons.
    Signed-off-by: koa2019 <no-reply>

--- cppTemplate/main.cpp
+++ cppTemplate/main.cpp
@@ -6,6 +6,14 @@
 
 //System Libraries
 #include <bits/stdc++.h> 
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+#include <cctype>
+#include <fstream>
+#include <climits> // char and int maximums
+#include <cfloat>  //float maximum
 using namespace std;  //STD Name-space where Library is compiled
 
 //User Libraries
@@ -15,20 +23,24 @@ using namespace std;  //STD Name-space where Library is compiled
 
 //Function Prototypes
 
+
 //Code Begins Execution Here with function main
 int main(int argc, char** argv) {
     //Set random number seed once here
     srand(static_cast<unsigned int>(time(0)));
-    //rand()%90+10;//2 digit numbers
+    //rand()%90+10;//2 digit numbers    
     
-    //Declare variables here
-        
+    //Declare variables here       
+    
+    //Initialize Variables    
+    
+    //Map inputs to outputs -> i.e. process the inputs
+   
     //Initialize variables here
     
     //Map inputs to outputs here, i.e. the process
     
     //Display the results
-    cout<<"Hello World\n";
     return 0;
 }
 
